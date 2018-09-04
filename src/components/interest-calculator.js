4e27c46123bb5217cf342b464afe5cc162b40dc1
Glitch (redux-interest-calculator-disconnected) <none>
2018-09-04 11:42:03 -0500

    connected interest-calculator to Redux store

--- src/components/interest-calculator.js
+++ src/components/interest-calculator.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
 // Connect this component
-export default function InterestCalculator(props) {
+export function InterestCalculator(props) {
     return (
         <form className="interest-calculator"
             onSubmit={e => e.preventDefault()}>
             <div className="form-group">
                 <label htmlFor="principal">Principal ($)</label>
                 <input type="number" id="principal" value={props.principal}
-                    min="0" />
+                    min="0" onChange={(e) => props.dispatch(actions.setPrincipal(e.target.value))} />
             </div>
             <div className="form-group">
                 <label htmlFor="interest">Interest rate (%)</label>
                 <input type="number" id="interest" value={props.interest}
-                    min="0" max="100" step="0.1" />
+                    min="0" max="100" step="0.1"
+                    onChange={(e) => props.dispatch(actions.setInterest(e.target.value))} />
             </div>
             <div className="form-group">
                 <label htmlFor="years">Years</label>
                 <input type="number" id="years" value={props.years}
-                    min="0" max="100" />
+                    min="0" max="100"
+                    onChange={(e) => props.dispatch(actions.setYears(e.target.value))} />
             </div>
             <div className="form-group">
                 <label htmlFor="total">Total</label>
@@ -34,3 +38,12 @@ InterestCalculator.defaultProps = {
     years: 0,
     total: 0
 };
+
+const mapStateToProps = (state, props) => ({
+    principal: state.principal,
+    interest: state.interest,
+    years: state.years,
+    total: state.principal * Math.pow(1 + state.interest / 100, state.years)
+});
+
+export default connect(mapStateToProps)(InterestCalculator);
